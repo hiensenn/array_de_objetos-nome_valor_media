cd4cc1d967caaa79b0bb803b539284074d9457ac
HiensenLima <hiensen.lima@sienacompany.com>
2022-11-21 01:49:20 -0300

    sistemas de notas OO

---
+++ Estudos/Faculdade/Boletim.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faculdade
+{
+    internal class Boletim
+    {
+        public string Disciplina { get; set; } 
+        public double[] Notas { get; set; }
+    
+        public double Media { get; set; }
+
+        public void calculoMedia()
+        {
+            double somaNotas = 0.0;
+
+            for(int i = 0; i < Notas.Length; i++)
+            {
+
+                somaNotas += Notas[i]; 
+            }
+
+            Media = somaNotas / Notas.Length;
+        }
+        //public void pesoDisc()
+        //{
+        //    Media = ((Nota1 * 3) + (Nota2 * 3) + (Nota3 * 4)) / 10;
+        //}
+        public string resultado()
+        {
+            if(Media >= 7)
+            {
+                return "Aprovado!";
+            }
+            else if(Media >= 5 )
+            {
+                return "Recuperação!";
+            }
+            else
+            {
+                return "Reprovado";
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ Estudos/Faculdade/Program.cs
@@ -0,0 +1,58 @@
+﻿using Faculdade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, qtdProvas;
+            Boletim[] boletim = null;
+            
+
+            Console.Write("Quantas disciplinas você está fazendo : ");
+            n = int.Parse(Console.ReadLine());
+
+            boletim = new Boletim[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                
+                boletim[i] = new Boletim();
+
+                Console.Write("Insira a disciplina : ");
+                boletim[i].Disciplina = Console.ReadLine();
+
+                Console.Write("Insira Quantas provas você fez nesta disciplina : ");
+                qtdProvas = int.Parse(Console.ReadLine());
+
+                boletim[i].Notas = new double[qtdProvas];
+
+                for (int j = 0; j < qtdProvas; j++)
+                {
+                    
+                    Console.Write($"Insira a sua {j+1}ª nota : ");
+                    double aux = (double.Parse(Console.ReadLine()));
+                    boletim[i].Notas[j] = aux;
+
+
+                }
+
+                boletim[i].calculoMedia();
+                Console.WriteLine($"{boletim[i].resultado()}");
+            }
+
+            
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
